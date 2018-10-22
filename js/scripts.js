e6b34c4985c0d99b02627fc36a1b6458ac794a96
Gulzat Karimova and Kenny Wolfenberger <gulzat@>
2018-10-22 14:41:56 -0700

    Created grocery list using arrays and forEach method

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,18 @@
 
 $(document).ready(function() {
-  $("#formOne").submit(function(event) {
+  $(".groceries").submit(function(event) {
 
     event.preventDefault();
+    var str = $("input#groceryList").val();
+    var formNeeds = str.split(" ");
+    formNeeds.sort();
+    formNeeds.forEach(function(formNeed) {
+      var userInput = $("#groceryList" + formNeed).val();
+
+      $(".list").append("<li>" + formNeed + "</li>");
+      $("#groceryList").hide();
+
+    })
+
   });
 });
